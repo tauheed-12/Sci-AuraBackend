65e5185afbef8b54ecffb131c9510f06b4a36d10
tauheed-12 <mohammadtauheed160@gmail.com>
2024-04-26 12:42:12 +0530

    eight

--- Server.js
+++ Server.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'https://main--sicu-aura-bytauheeds.netlify.app/', 
   credentials: true
 }));
 
